29c589588554a0e12c328e8ea4c4525d554015de
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-06-28 12:36:23 +0800

    - Reading data From Database Using Query Set

--- 8-QuerySet/blog/urls.py
+++ 8-QuerySet/blog/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('post/', views.post),
+    path('', views.post),
+    path('berita/', views.berita),
+    path('how_to/', views.how_to),
+    path('gosip/', views.gosip),
 ]
--- 8-QuerySet/blog/views.py
+++ 8-QuerySet/blog/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render
-
-
-def index(request):
-    data = {
-        'title': "Halaman Blog",
-        'heading': "Selamat datang di Blog",
-    }
-    return render(request, 'blog/index.html', data)
+from .models import Post
 
 
 def post(request):
+    posts = Post.objects.all()
     data = {
-        'title': "Halaman Blog Post",
+        'title': "Halaman Blog",
         'heading': "Selamat datang di Blog Post",
+        'posts': posts,
     }
     return render(request, 'blog/post.html', data)
+
+
+def berita(request):
+    berita = Post.objects.filter(category='Berita')
+    data = {
+        'title': "Halaman Berita",
+        'heading': "Selamat datang di Berita",
+        'posts': berita,
+    }
+    return render(request, 'blog/berita.html', data)
+
+
+def how_to(request):
+    how_to = Post.objects.filter(category='How To')
+    data = {
+        'title': "Halaman How To",
+        'heading': "Selamat datang di Halaman How To",
+        'posts': how_to,
+    }
+    return render(request, 'blog/howto.html', data)
+
+
+def gosip(request):
+    gosip = Post.objects.filter(category='Gosip')
+    data = {
+        'title': "Halaman Gosip",
+        'heading': "Gosip Hangat",
+        'posts': gosip,
+    }
+    return render(request, 'blog/gosip.html', data)
