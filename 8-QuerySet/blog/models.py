040e35f95218a6ce7be1331a910fb4cf9d19d315
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-06-28 12:03:58 +0800

    - Menambahkan Model Post pada Project - Meregister Model Post pada Admin Site

--- 8-QuerySet/blog/models.py
+++ 8-QuerySet/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    category = models.CharField(max_length=255)
+    post_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}. {}".format(self.id, self.title)
